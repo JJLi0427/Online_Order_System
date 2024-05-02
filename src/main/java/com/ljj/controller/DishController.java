a62c3f234b51545574010723d0d374922ce4bb2f
LJJ-win <2366876022@qq.com>
2024-05-02 15:07:18 +0800

    new

--- src/main/java/com/ljj/controller/DishController.java
+++ src/main/java/com/ljj/controller/DishController.java
@@ -1,15 +1,25 @@
 package com.ljj.controller;
 
+import com.ljj.service.DishService;
+import com.ljj.pojo.Dish;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
 
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/dish")
 public class DishController {
-    
-    @RequestMapping()
-    public String list(){
-        return "index";
+    @Autowired
+    private DishService dishService;
+
+    @RequestMapping("/alldish")
+    public String list(Model model){
+        List<Dish> list = dishService.list();
+        model.addAttribute("list", list);
+        return "alldish";
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -6,13 +6,12 @@
 </head>
 <body>
     <div class="container">
-        <div class="image-container">
-            <img src="photo/home.jpg" alt="Home">
-        </div>
-        <div class="content">
-            <h1>在线点餐系统</h1>
-            <button class="button">顾客登录</button>
-            <button class="button">店员登录</button>
+        <div class="glass-effect">
+            <h1 class="title">在线点餐系统</h1>
+            <div class="button-container">
+                <a href="userlogin.jsp" class="button">顾客登录</a>
+                <a href="stafflogin.jsp" class="button">后台登录</a>
+            </div>
         </div>
     </div>
 </body>
